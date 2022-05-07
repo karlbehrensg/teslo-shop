b3745a4a1da36917b49884f650b0361c0e212832
Karl Behrens <karlbehrensg@gmail.com>
2022-05-07 13:52:59 -0400

    Product page

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,10 +1,12 @@
 import { FC, useMemo, useState } from "react";
+import NextLink from "next/link";
 import {
   Box,
   Card,
   CardActionArea,
   CardMedia,
   Grid,
+  Link,
   Typography,
 } from "@mui/material";
 import { IProduct } from "../../interfaces";
@@ -31,14 +33,18 @@ export const ProductCard: FC<Props> = ({ product }) => {
       onMouseLeave={() => setIsHovered(false)}
     >
       <Card>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            className="fadeIn"
-            image={productImage}
-            alt={product.title}
-          />
-        </CardActionArea>
+        <NextLink href="/product/slug" passHref prefetch={false}>
+          <Link>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                className="fadeIn"
+                image={productImage}
+                alt={product.title}
+              />
+            </CardActionArea>
+          </Link>
+        </NextLink>
       </Card>
       <Box sx={{ mt: 1 }} className="fadeIn">
         <Typography fontWeight={700}>{product.title}</Typography>
