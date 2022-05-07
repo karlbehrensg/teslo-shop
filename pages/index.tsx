c56fd4e6a6ffcd67b1059606d5e56bc29879d78c
Karl Behrens <karlbehrensg@gmail.com>
2022-05-06 22:50:06 -0400

    Directory and layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
+import { Typography } from "@mui/material";
 import type { NextPage } from "next";
+import { ShopLayout } from "../components/layout";
 
 const Home: NextPage = () => {
-  return <h1>Teslo-Shop</h1>;
+  return (
+    <ShopLayout
+      title={"Teslo-Shop"}
+      pageDescription={"Find the best Teslo products"}
+    >
+      <Typography variant="h1">Shop</Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        All products
+      </Typography>
+    </ShopLayout>
+  );
 };
 
 export default Home;
