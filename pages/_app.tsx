0571edab1a69a3e072aca619f8e2c0b703d26f9e
Karl Behrens <karlbehrensg@gmail.com>
2022-05-06 23:27:30 -0400

    Navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+
 import { lightTheme } from "../themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
