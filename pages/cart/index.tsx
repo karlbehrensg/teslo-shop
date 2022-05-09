a03b84aa29f7911d1d30751968ff7f780d5c11a3
Karl Behrens <karlbehrensg@gmail.com>
2022-05-09 00:38:59 -0400

    Cart page layout

---
+++ pages/cart/index.tsx
@@ -0,0 +1,41 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { ShopLayout } from "../../components/layout/ShopLayout";
+import { CartList } from "../../components/cart";
+
+const CartPage = () => {
+  return (
+    <ShopLayout title="Cart - 3" pageDescription="Cart">
+      <Typography variant="h1" component="h1">
+        Cart
+      </Typography>
+      <Grid container>
+        <Grid item xs={12} sm={7}>
+          <CartList />
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          <Card className="summary-card">
+            <CardContent>
+              <Typography variant="h2">Order</Typography>
+              <Divider sx={{ my: 1 }} />
+              <Box sx={{ mt: 3 }}>
+                <Button color="secondary" className="circular-btn" fullWidth>
+                  Checkout
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </ShopLayout>
+  );
+};
+
+export default CartPage;
